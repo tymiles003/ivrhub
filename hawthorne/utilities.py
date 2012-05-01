ddff781f6ea0a72297528c47128faeca7d41a3fc
Matt Ball <yosemitebandit@gmail.com>
2012-04-30 23:58:28 -0700

    abstracts out utility functions

---
+++ hawthorne/utilities.py
@@ -0,0 +1,126 @@
+''' utility functions
+'''
+import os
+import urlparse
+
+import boto
+from flask import (url_for)
+
+from hawthorne import app
+
+def generate_random_string(length):
+    ''' generating API IDs and keys
+    '''
+    # technique from: http://stackoverflow.com/questions/2898685
+    return ''.join(
+        map(lambda x: '0123456789abcdefghijklmnopqrstuvwxyz'[ord(x)%36]
+        , os.urandom(length)))
+
+        
+def send_forgot_password_link(user):
+    ''' user has requested password reset link
+    '''
+    forgot_url = urlparse.urljoin(app.config['APP_ROOT']
+        , url_for('forgot_password', code=user.forgot_password_code))
+
+    body = '''
+        Hello!  Someone has recently requested a password reset for %s.  
+        If that was not you, please disregard this message.  
+        
+        If it was you, please click this link to choose a new password, thanks!
+        
+        %s
+         ''' % (app.config['APP_NAME'], forgot_url)
+
+    _send_email(user.email
+        , 'Re: requested %s password reset' % app.config['APP_NAME']
+        , body)
+    
+
+def send_notification_of_verification(user, email):
+    ''' email a user that they've been verified by an admin and now have full
+    access to the site
+    '''
+    if not email:
+        app.logger.error('no verification email specified for %s.  \
+            email on record is %s' % (user.name, user.email))
+    else:
+        body = '''
+            Hello!  Your information has been verified by an administrator and
+            you now have full access to %s.
+            
+            Just wanted to let you know, thanks!
+
+            %s
+            ''' % (app.config['APP_NAME'], app.config['APP_ROOT'])
+
+        _send_email(user.email
+            , 'you now have access to %s' % app.config['APP_NAME']
+            , body)
+
+
+def send_admin_verification(user):
+    ''' email to an admin indicating that there a user needs verification
+    '''
+    directory_url = urlparse.urljoin(app.config['APP_ROOT']
+        , url_for('directory', internal_id=user._id))
+
+    body = '''
+        Howdy!  Someone new has confirmed his or her email address and now 
+        needs to be verified by an admin.  Here are the details:
+
+        Name: %s
+        Email: %s
+        Organization: %s
+
+        Click the following link to edit the verification status of this
+        person.
+
+        %s
+
+        Thanks!
+        ''' % (user.name, user.email, user.organization, directory_url)
+
+    # send to the AWS verified sender
+    # ..should probably use a manager's email instead
+    _send_email( app.config['AWS']['verified_sender']
+        , '%s | %s needs to be verified' % (app.config['APP_NAME'], user.name)
+        , body)
+
+
+def send_confirmation_email(user):
+    ''' sends an email to a newly-registered user with a confirmation code
+    '''
+    confirmation_url = urlparse.urljoin(app.config['APP_ROOT']
+        , url_for('confirm_email', code=user.email_confirmation_code))
+
+    body = '''
+        Hello, this email was recently used to sign up for an account with 
+        %s.  If it was you that signed up for this account, please click
+        the link below to confirm your email address.
+        
+        If you did not sign up for this account, please disregard this message.
+        
+        %s
+
+        Thanks!
+        ''' % (app.config['APP_NAME'], confirmation_url)
+
+    _send_email( user.email
+        , '%s email confirmation' % app.config['APP_NAME']
+        , body)
+
+
+def _send_email(recipient, subject, body):
+    ''' send an email using SES from the config's verified sender
+    '''
+    connection = boto.connect_ses(
+        aws_access_key_id=app.config['AWS']['access_key_id']
+        , aws_secret_access_key=app.config['AWS']['secret_access_key'])
+
+    result = connection.send_email(
+        app.config['AWS']['verified_sender']
+        , subject
+        , body
+        , [recipient])
+    # need to catch errors in result
--- hawthorne/views.py
+++ hawthorne/views.py
@@ -3,10 +3,7 @@ the dynamic routes..how fun
 '''
 import datetime
 from functools import wraps
-import os
-import urlparse
 
-import boto
 from flask import (render_template, request, flash, redirect, url_for, session
     , abort)
 from flaskext.bcrypt import Bcrypt
@@ -16,6 +13,7 @@ from hawthorne import app
 bcrypt = Bcrypt(app)
 
 from models import *
+import utilities
 
 
 ''' decorators
@@ -108,10 +106,11 @@ def register():
         try:
             new_user = User(
                 admin_rights = False
-                , api_id = 'ID' + _generate_random_string(32)
-                , api_key = _generate_random_string(34)
+                , api_id = 'ID' + utilities.generate_random_string(32)
+                , api_key = utilities.generate_random_string(34)
                 , email = request.form['email']
-                , email_confirmation_code = _generate_random_string(34)
+                , email_confirmation_code = \
+                    utilities.generate_random_string(34)
                 , email_confirmed = False
                 , last_login_time = datetime.datetime.utcnow()
                 , name = request.form['name']
@@ -129,7 +128,7 @@ def register():
             return redirect(url_for('register'))
         
         # seek email confirmation
-        _send_confirmation_email(new_user)
+        utilities.send_confirmation_email(new_user)
 
         # log the user in  
         session['email'] = new_user.email
@@ -156,7 +155,7 @@ def confirm_email(code):
     session['email'] = user.email
 
     # email an admin for verification
-    _send_admin_verification(user)
+    utilities.send_admin_verification(user)
         
     # redirect to a holding area
     return redirect(url_for('verification_status'))
@@ -272,7 +271,7 @@ def directory(internal_id):
                     app.logger.info('%s verified %s' % (session['email']
                         , request.form['email']))
                     # send email to user that they've been verified
-                    _send_notification_of_verification(user
+                    utilities.send_notification_of_verification(user
                         , request.form.get('email', ''))
                 user.verified = True
 
@@ -423,11 +422,11 @@ def forgot(code):
             return redirect(url_for('forgot'))
         
         user = user[0]
-        user.forgot_password_code = _generate_random_string(34)
+        user.forgot_password_code = utilities.generate_random_string(34)
         user.save()
         #user.reload()
 
-        _send_forgot_password_link(user)
+        utilities.send_forgot_password_link(user)
         
         app.logger.info('%s requested a forgotten password code' % \
             request.form['email'])
@@ -484,131 +483,13 @@ def csrf_protect():
 
 def _generate_csrf_token():
     if '_csrf_token' not in session:
-        session['_csrf_token'] = _generate_random_string(24)
+        session['_csrf_token'] = utilities.generate_random_string(24)
     return session['_csrf_token']
 
 app.jinja_env.globals['csrf_token'] = _generate_csrf_token
 
 
 
-''' utility functions
-'''
-def _generate_random_string(length):
-    ''' generating API IDs and keys
-    '''
-    # technique from: http://stackoverflow.com/questions/2898685
-    return ''.join(
-        map(lambda x: '0123456789abcdefghijklmnopqrstuvwxyz'[ord(x)%36]
-        , os.urandom(length)))
-
-        
-def _send_forgot_password_link(user):
-    ''' user has requested password reset link
-    '''
-    forgot_url = urlparse.urljoin(app.config['APP_ROOT']
-        , url_for('forgot_password', code=user.forgot_password_code))
-
-    body = '''
-        Hello!  Someone has recently requested a password reset for %s.  
-        If that was not you, please disregard this message.  
-        
-        If it was you, please click this link to choose a new password, thanks!
-        
-        %s
-         ''' % (app.config['APP_NAME'], forgot_url)
-
-    _send_email(user.email
-        , 'Re: requested %s password reset' % app.config['APP_NAME']
-        , body)
-    
-
-def _send_notification_of_verification(user, email):
-    ''' email a user that they've been verified by an admin and now have full
-    access to the site
-    '''
-    if not email:
-        app.logger.error('no verification email specified for %s.  \
-            email on record is %s' % (user.name, user.email))
-    else:
-        body = '''
-            Hello!  Your information has been verified by an administrator and
-            you now have full access to %s.
-            
-            Just wanted to let you know, thanks!
-
-            %s
-            ''' % (app.config['APP_NAME'], app.config['APP_ROOT'])
-
-        _send_email(user.email
-            , 'you now have access to %s' % app.config['APP_NAME']
-            , body)
-
-
-def _send_admin_verification(user):
-    ''' email to an admin indicating that there a user needs verification
-    '''
-    directory_url = urlparse.urljoin(app.config['APP_ROOT']
-        , url_for('directory', internal_id=user._id))
-
-    body = '''
-        Howdy!  Someone new has confirmed his or her email address and now 
-        needs to be verified by an admin.  Here are the details:
-
-        Name: %s
-        Email: %s
-        Organization: %s
-
-        Click the following link to edit the verification status of this
-        person.
-
-        %s
-
-        Thanks!
-        ''' % (user.name, user.email, user.organization, directory_url)
-
-    # send to the AWS verified sender
-    # ..should probably use a manager's email instead
-    _send_email( app.config['AWS']['verified_sender']
-        , '%s | %s needs to be verified' % (app.config['APP_NAME'], user.name)
-        , body)
-
-
-def _send_confirmation_email(user):
-    ''' sends an email to a newly-registered user with a confirmation code
-    '''
-    confirmation_url = urlparse.urljoin(app.config['APP_ROOT']
-        , url_for('confirm_email', code=user.email_confirmation_code))
-
-    body = '''
-        Hello, this email was recently used to sign up for an account with 
-        %s.  If it was you that signed up for this account, please click
-        the link below to confirm your email address.
-        
-        If you did not sign up for this account, please disregard this message.
-        
-        %s
-
-        Thanks!
-        ''' % (app.config['APP_NAME'], confirmation_url)
-
-    _send_email( user.email
-        , '%s email confirmation' % app.config['APP_NAME']
-        , body)
-
-
-def _send_email(recipient, subject, body):
-    ''' send an email using SES from the config's verified sender
-    '''
-    connection = boto.connect_ses(
-        aws_access_key_id=app.config['AWS']['access_key_id']
-        , aws_secret_access_key=app.config['AWS']['secret_access_key'])
-
-    result = connection.send_email(
-        app.config['AWS']['verified_sender']
-        , subject
-        , body
-        , [recipient])
-    # need to catch errors in result
 
 
 
@@ -683,10 +564,10 @@ def initialize_database():
 
     default_admin = User(
         admin_rights = True
-        , api_id = 'ID' + _generate_random_string(32)
-        , api_key = _generate_random_string(34)
+        , api_id = 'ID' + utilities.generate_random_string(32)
+        , api_key = utilities.generate_random_string(34)
         , email = initial_user['email']
-        , email_confirmation_code = _generate_random_string(34)
+        , email_confirmation_code = utilities.generate_random_string(34)
         , email_confirmed = False
         , last_login_time = datetime.datetime.utcnow()
         , name = initial_user['name']
