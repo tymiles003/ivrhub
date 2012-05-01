473b1cdc880098b8d4ea7f50f835ca98c1290b78
Matt Ball <yosemitebandit@gmail.com>
2012-05-01 09:34:44 -0700

    pulls out decorators

---
+++ hawthorne/decorators.py
@@ -0,0 +1,66 @@
+''' decorators
+'''
+from functools import wraps
+
+from flask import (session, redirect, request, url_for, abort)
+from mongoengine import *
+
+from hawthorne import app
+from models import *
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'email' not in session:
+            app.logger.warning(
+                'someone tried to access %s, a login-only page' % request.url)
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+def verification_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # check that someone is logged in
+        if 'email' not in session:
+            app.logger.warning(
+                'someone tried to access %s, a login-only page' % request.url)
+            return redirect(url_for('login'))
+        # check verification
+        user = User.objects(email=session['email'])[0]
+        if not user.verified:
+            app.logger.warning('%s tried to access %s, a verified-only page' \
+                % (session['email'], request.url))
+            return redirect(url_for('verification_status'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+def admin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # check that someone is logged in
+        if 'email' not in session:
+            app.logger.warning(
+                'someone tried to access %s, a login-only page' % request.url)
+            return abort(404)
+        # check admin status
+        user = User.objects(email=session['email'])[0]
+        if not user.admin_rights:
+            app.logger.warning('%s tried to access %s, an admin-only page' % (
+                session['email'], request.url))
+            return redirect(url_for('home'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+def require_not_logged_in(f):
+    ''' redirect if user is logged in already
+    '''
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'email' in session:
+            return redirect(url_for('dashboard'))
+        return f(*args, **kwargs)
+    return decorated_function
--- hawthorne/views.py
+++ hawthorne/views.py
@@ -2,7 +2,6 @@
 the dynamic routes..how fun
 '''
 import datetime
-from functools import wraps
 
 from flask import (render_template, request, flash, redirect, url_for, session
     , abort)
@@ -12,6 +11,8 @@ from mongoengine import *
 from hawthorne import app
 bcrypt = Bcrypt(app)
 
+# view controls
+from decorators import *
 # jinja filters and CSRF
 from filters import *
 # mongoengine models
@@ -22,66 +23,6 @@ from seed import *
 import utilities
 # boring routes
 from vanilla_views import *
-
-
-''' decorators
-'''
-def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if 'email' not in session:
-            app.logger.warning(
-                'someone tried to access %s, a login-only page' % request.url)
-            return redirect(url_for('login'))
-        return f(*args, **kwargs)
-    return decorated_function
-
-
-def verification_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        # check that someone is logged in
-        if 'email' not in session:
-            app.logger.warning(
-                'someone tried to access %s, a login-only page' % request.url)
-            return redirect(url_for('login'))
-        # check verification
-        user = User.objects(email=session['email'])[0]
-        if not user.verified:
-            app.logger.warning('%s tried to access %s, a verified-only page' \
-                % (session['email'], request.url))
-            return redirect(url_for('verification_status'))
-        return f(*args, **kwargs)
-    return decorated_function
-
-
-def admin_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        # check that someone is logged in
-        if 'email' not in session:
-            app.logger.warning(
-                'someone tried to access %s, a login-only page' % request.url)
-            return abort(404)
-        # check admin status
-        user = User.objects(email=session['email'])[0]
-        if not user.admin_rights:
-            app.logger.warning('%s tried to access %s, an admin-only page' % (
-                session['email'], request.url))
-            return redirect(url_for('home'))
-        return f(*args, **kwargs)
-    return decorated_function
-
-
-def require_not_logged_in(f):
-    ''' redirect if user is logged in already
-    '''
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if 'email' in session:
-            return redirect(url_for('dashboard'))
-        return f(*args, **kwargs)
-    return decorated_function
 
 
 ''' routes
