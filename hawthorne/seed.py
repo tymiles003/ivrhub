eb11ee4a85f6c89646ff654735008b2327592502
Matt Ball <yosemitebandit@gmail.com>
2012-05-03 11:15:07 -0700

    only seed a user if no users exist yet

--- hawthorne/seed.py
+++ hawthorne/seed.py
@@ -14,7 +14,6 @@ def seed():
     ''' adds a default admin to the system based on your settings
     usage:
         $ . /path/to/venv/bin/activate
-        $ cd /path/to/hawthorne
         $ python
         >> import hawthorne
         >> hawthorne.views.seed()
@@ -26,28 +25,33 @@ def seed():
         , host=app.config['MONGO_CONFIG']['host']
         , port=int(app.config['MONGO_CONFIG']['port']))
 
-    initial_user = app.config['INITIAL_USER']
+    users = User.objects()
+    if not users:
+        app.logger.info('no users in the db, so we will create one')
+        initial_user = app.config['INITIAL_USER']
 
-    default_admin = User(
-        admin_rights = True
-        , api_id = 'ID' + utilities.generate_random_string(32)
-        , api_key = utilities.generate_random_string(34)
-        , email = initial_user['email']
-        , email_confirmation_code = utilities.generate_random_string(34)
-        , email_confirmed = False
-        , last_login_time = datetime.datetime.utcnow()
-        , name = initial_user['name']
-        , organization = initial_user['organization']
-        , password_hash = bcrypt.generate_password_hash(
-            initial_user['password'])
-        , registration_time = datetime.datetime.utcnow()
-        , verified = True
-    )
-    try:
-        default_admin.save()
-        app.logger.info('user %s created with specified password' % \
-            default_admin['email'])
-    except:
-        app.logger.error('user %s could not be saved; check for dupes' % \
-            default_admin['email'])
+        default_admin = User(
+            admin_rights = True
+            , api_id = 'ID' + utilities.generate_random_string(32)
+            , api_key = utilities.generate_random_string(34)
+            , email = initial_user['email']
+            , email_confirmation_code = utilities.generate_random_string(34)
+            , email_confirmed = False
+            , last_login_time = datetime.datetime.utcnow()
+            , name = initial_user['name']
+            , organization = initial_user['organization']
+            , password_hash = bcrypt.generate_password_hash(
+                initial_user['password'])
+            , registration_time = datetime.datetime.utcnow()
+            , verified = True
+        )
+        try:
+            default_admin.save()
+            app.logger.info('user %s created with specified password' % \
+                default_admin['email'])
+        except:
+            app.logger.error('user %s could not be saved; check for dupes' % \
+                default_admin['email'])
+    else:
+        app.logger.info('not seeding db as there are already users present')
 
