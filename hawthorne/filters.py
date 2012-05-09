ff74e22dcd9e9324752a06022b941171cf45eadc
Matt Ball <yosemitebandit@gmail.com>
2012-05-09 13:44:20 -0700

    adds jinja filter for abbreviation

--- hawthorne/filters.py
+++ hawthorne/filters.py
@@ -7,6 +7,32 @@ from hawthorne import app
 import utilities
 
 
+@app.template_filter('abbreviate')
+def abbreviate(word, length, remove='end'):
+    ''' turns long phrases into something like 'asdf123..'
+    the 'remove' parameter determines which part of the string to drop
+    usage in jinja template:
+        call ID: {{ response.call_sid }}
+        call ID: {{ response.call_sid|abbreviate(7, remove='start') }}
+    yields something like:
+        call ID: SIDabcdefgh1234
+        call ID: ..fgh1234
+    '''
+    if not word:
+        return word
+    
+    word = str(word)
+    if len(word) <= length:
+        return word
+
+    if remove == 'start':
+        # drop the beginning of the word
+        return '..' + word[len(word)-length:]
+    else:
+        # drop the end
+        return word[0:length] + '..'
+
+
 @app.template_filter('_format_datetime')
 def _format_datetime(dt, formatting='medium'):
     ''' jinja filter for displaying datetimes
